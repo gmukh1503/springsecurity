444e20cdeb2037421cfa8567919d16b39ac19e04
Gourab Mukherjee <gourab.mukherjee3@gmail.com>
2020-04-27 19:16:07 +0530

    spring-security-github-login

---
+++ spring-security-github-login/src/main/java/com/gourab/learning/springsecuritygooglelogin/SpringSecurityGitHubLoginApplication.java
@@ -0,0 +1,15 @@
+package com.gourab.learning.springsecuritygooglelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SpringSecurityGitHubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGitHubLoginApplication.class, args);
+	}
+
+}
